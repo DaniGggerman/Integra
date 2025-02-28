20060dc371f46b1293070004624a20a083317b71
Hero <160369015+hproxima@users.noreply.github.com>
2025-02-28 09:50:53 +0800

    nega

--- dB/config.php
+++ dB/config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "mananggit";
+$database = "users";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
