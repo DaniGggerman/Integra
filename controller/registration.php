6669734424c685abca9b4534e0e0fb79a6f76074
Hero <160369015+hproxima@users.noreply.github.com>
2025-03-12 14:41:07 +0800

    Updated registration script and checked for changes in the folder

--- controller/registration.php
+++ controller/registration.php
@@ -2,43 +2,44 @@
 include("../dB/config.php");
 session_start();
 
-if(isset($_POST['registration'])){
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$cpassword = $_POST['cpassword'];
-$phoneNumber = $_POST['phoneNumber'];
-$gender = $_POST['gender'];
-$birthday = $_POST['birthday'];
+if (isset($_POST['registration'])) {
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $gender = $_POST['gender'];
+    $birthday = $_POST['birthday'];
 
-if($password != $cpassword){
-    $_SESSION['message'] = "Password and Confirm Password does not match";
-    $_SESSION['code'] = "error";
-    header("Location: ../registration.php");
-    exit(0);
-}
+    if ($password != $cpassword) {
+        $_SESSION['message'] = "Password and Confirm Password do not match";
+        $_SESSION['code'] = "error";
+        header("Location: ../registration.php");
+        exit(0);
+    }
 
-$query = "SELECT * FROM `users` WHERE `email` = '$email'";
-$result = mysqli_query($conn, $query);
+    $query = "SELECT * FROM `users` WHERE `email` = '$email'";
+    $result = mysqli_query($conn, $query);
 
-if(mysqli_num_rows($result) > 0){
-    $_SESSION['message'] = "Email Address already exists";
-    $_SESSION['code'] = "error";
-    header("Location: ../registration.php");
-    exit(0);
-}
+    if (mysqli_num_rows($result) > 0) {
+        $_SESSION['message'] = "Email Address already exists";
+        $_SESSION['code'] = "error";
+        header("Location: ../registration.php");
+        exit(0);
+    }
 
-$query = "INSERT INTO `users` (`firstName`, `lastName`, `email`, `password`, `phoneNumber`, `gender`, `birthday`) VALUES ('$firstName', '$lastName', '$email', '$password', '$phoneNumber', '$gender', '$birthday')";
+    $query = "INSERT INTO `users` (`firstName`, `lastName`, `email`, `password`, `phoneNumber`, `gender`, `birthday`) VALUES ('$firstName', '$lastName', '$email', '$password', '$phoneNumber', '$gender', '$birthday')";
 
-if(mysqli_query($conn, $query)){
-    $_SESSION['message'] = "Registered Successfully";
-    $_SESSION['code'] = "success";
-    header("Location: ../registration.php");
-    exit(0);
-}else{
-    echo "Error:" . mysqli_error($conn);
-}
-mysqli_close($conn);
+    if (mysqli_query($conn, $query)) {
+        $_SESSION['message'] = "Registered Successfully";
+        $_SESSION['code'] = "success";
+        header("Location: ../registration.php");
+        exit(0);
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 }
 ?>
--- dB/config.php
+++ dB/config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "aguaviva_baraocor";
+$database ="aguaviva_baraocor";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
